48b6ddfb85f37f9cd47b5776f10561c428a626c7
Nikolai <nlv.nki@gmail.com>
2022-02-21 12:48:34 +0300

    generateRange

---
+++ generateRange.js
@@ -0,0 +1,8 @@
+const generateRange = (min,max,step) => {
+    const range = [];
+    for (let i = min; i <= max;i += step) {
+        range.push(i);
+    }
+    return result;
+}
+console.log(generateRange(2,10,3));
